7906990e611180f1e94c9c35b9f3de018f4455f7
Shengyu Wang <shengyuwang95@gmail.com>
2021-01-15 10:33:38 +1100

    Add snake following anination

--- game.js
+++ game.js
@@ -1,10 +1,14 @@
-import {SNAME_SPEED, update as updateSnake, draw as drawSnake} from './snake.js'
+import {
+    SNAME_SPEED,
+    update as updateSnake,
+    draw as drawSnake
+} from './snake.js'
 let lastRenderTime = 0
 const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
     window.requestAnimationFrame(main)
-    const secondsSinceLastRender =( currentTime - lastRenderTime) / 1000 // Time elapsed in seconds
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000 // Time elapsed in seconds
     if (secondsSinceLastRender < 1 / SNAME_SPEED) return
 
     lastRenderTime = currentTime
@@ -18,6 +22,8 @@ window.requestAnimationFrame(main)
 function update() {
     updateSnake()
 }
+
 function draw() {
+    gameBoard.innerHTML = ''
     drawSnake(gameBoard)
 }
--- snake.js
+++ snake.js
@@ -1,14 +1,41 @@
 export const SNAME_SPEED = 1 // Number of render per second
-const snakeBody = [{x: 11, y:11}] // Coordinate in a grid
+const snakeBody = [{
+        x: 10,
+        y: 11
+    },
+    {
+        x: 11,
+        y: 11
+    },
+    {
+        x: 12,
+        y: 11
+    },
+    {
+        x: 13,
+        y: 11
+    },
+    {
+        x: 14,
+        y: 11
+    },
+] // Coordinate in a grid
 
 export function update() {
-    console.log('update snake')
+    for (let i = snakeBody.length - 2; i >= 0; i--) {
+        snakeBody[i + 1] = {
+            ...snakeBody[i]
+        }
+    }
+
+    snakeBody[0].x += 0
+    snakeBody[0].y += 1
 }
 export function draw(gameBoard) {
     snakeBody.forEach(segment => {
         const snakeElement = document.createElement('div')
-        snakeElement.style.gridRowStart = segment.x
-        snakeElement.style.gridColumnStart = segment.y
+        snakeElement.style.gridColumnStart = segment.x
+        snakeElement.style.gridRowStart = segment.y
         snakeElement.classList.add('snake')
         gameBoard.appendChild(snakeElement)
     })
