f7bb6dce01fc0edefbe66c90ed09c0a927e8e4d1
Shengyu Wang <shengyuwang95@gmail.com>
2021-01-15 10:09:33 +1100

    Setup requestAnimationFrame

---
+++ game.js
@@ -0,0 +1,22 @@
+import {SNAME_SPEED, update as updateSnake, draw as drawSnake} from './snake.js'
+let lastRenderTime = 0
+
+function main(currentTime) {
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender =( currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAME_SPEED) return  
+
+    lastRenderTime = currentTime
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+}
+function draw() {
+    drawSnake()
+}
