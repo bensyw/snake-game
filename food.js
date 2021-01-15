88e00daa6bb092a387f04a164e803623d2667337
Shengyu Wang <shengyuwang95@gmail.com>
2021-01-15 11:21:08 +1100

    Randomize food position

--- food.js
+++ food.js
@@ -3,20 +3,18 @@ import {
     expandSnake
 } from './snake.js'
 
-let food = {
-    x: 10,
-    y: 1
-}
+import {
+    randomGridPosition
+} from './grid.js'
+
+let food = getRandomFoodPosition()
 
 const EXPANSION_RATE = 1
 
 export function update() {
     if (onSnake(food)) {
         expandSnake(EXPANSION_RATE)
-        food = {
-            x: 20,
-            y: 10
-        }
+        food = getRandomFoodPosition()
     }
 }
 
@@ -26,4 +24,12 @@ export function draw(gameBoard) {
     foodElement.style.gridRowStart = food.y
     foodElement.classList.add('food')
     gameBoard.appendChild(foodElement)
+}
+
+function getRandomFoodPosition() {
+    let newFoodPosition
+    while (newFoodPosition == null || onSnake(newFoodPosition)) {
+        newFoodPosition = randomGridPosition()
+    }
+    return newFoodPosition
 }
