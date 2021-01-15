9eab7f9c1d7e66afa2a586ab1f4242caabac5c86
Shengyu Wang <shengyuwang95@gmail.com>
2021-01-15 11:32:42 +1100

    Add checkDeath

--- game.js
+++ game.js
@@ -1,8 +1,14 @@
 import {
     SNAME_SPEED,
     update as updateSnake,
-    draw as drawSnake
+    draw as drawSnake,
+    getSnakeHead,
+    snakeIntersection
 } from './snake.js'
+
+import {
+    outsideGrid
+} from './grid.js'
 
 import {
     update as updadeFood,
@@ -11,9 +17,17 @@ import {
 
 
 let lastRenderTime = 0
+let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
+    if (gameOver) {
+        if (confirm('You lost. Press OK to restart.')) {
+            window.location.reload()
+        }
+        return
+    }
+
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000 // Time elapsed in seconds
     if (secondsSinceLastRender < 1 / SNAME_SPEED) return
@@ -29,10 +43,15 @@ window.requestAnimationFrame(main)
 function update() {
     updateSnake()
     updadeFood()
+    checkDeath()
 }
 
 function draw() {
     gameBoard.innerHTML = ''
     drawSnake(gameBoard)
     drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
